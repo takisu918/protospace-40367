1eff8bbd7f9469ae52dbdd1ab9d90b656dfe9a53
takisu918 <ta.kiss.ryokushakeiba0930@gmail.com>
2024-04-30 22:46:14 +0900

    protospace-40367の再提出用の修正⑧

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -51,4 +51,10 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
 end
